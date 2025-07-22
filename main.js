98bb7016be53889d9c579696dca4a623ca804f3f
yunus-emre-yaman <yunusemreyaman359@gmail.com>
2025-07-22 09:17:01 +0300

    Challenge 3 tamamlandı

--- main.js
+++ main.js
@@ -26,6 +26,31 @@ Sayfa aktifken (herhangi bir yere tıklandığında), klavye dinlemeye başlası
 NOT: Klavye eventlerini document yerine **window** nesnesine ekleyin.
 */
 
+let isListening = false;
+
+document.addEventListener("click", () => {
+  if (!isListening) {
+    window.addEventListener("keydown", handleKeyPress);
+    isListening = true;
+  }
+});
+
+function handleKeyPress(event) {
+  const body = document.body;
+  const themes = ["theme1", "theme2", "theme3"];
+
+  body.classList.remove(...themes);
+
+  if (event.key === "1") {
+    body.classList.add("theme1");
+  } else if (event.key === "2") {
+    body.classList.add("theme2");
+  } else if (event.key === "3") {
+    body.classList.add("theme3");
+  } else if (event.key === "Escape") {
+  }
+}
+
 /*
 CHALLENGE 3:
 <input id="full_name"> alanına yazı yazıldıkça:
@@ -33,6 +58,21 @@ CHALLENGE 3:
 - Metin 5 karakterden UZUNSA <button> elementini "enabled" yapın
 - Aksi halde (5 veya daha az karakter) buton "disabled" olmalı
 */
+
+const input = document.getElementById("full_name");
+const button = document.querySelector("button");
+
+input.addEventListener("input", () => {
+
+  input.value = input.value.toUpperCase();
+
+  if (input.value.length > 5) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+});
+
 
 /*
 CHALLENGE 4:
